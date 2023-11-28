c7bfc3ebbcae3207af3d0b5457d92467697b58a8
Aman Agrawal <Aman Agrawal>
2023-11-28 13:23:15 +0530

    Starting with authentication

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
+
+app.set('view engine', 'ejs');
+
+const dbURI = 'mongodb+srv://aman_cf:test1234@cluster0.oji0xpr.mongodb.net/node-auth?retryWrites=true&w=majority';
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
